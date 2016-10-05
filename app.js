7262a8dc288f32326be2d286d447648c3beb0961
CaillaudPA <caillaud.pierreantoine@yahoo.com>
2016-10-05 20:13:18 +0200

    EPSI WORK

--- app.js
+++ app.js
@@ -3,18 +3,15 @@ var app = express();
 
 //Définition du moteur de template
 app.set('view engine', 'ejs')
-
 app.use('/assets', express.static('public'))
-
-app.get('/', (request, response) => {
-	response.render("accueil")
-})
 
 var routes = require('./route/route')
 app.use('/', routes)
 
+
+
 app.listen(3000, (res, req) => {
-	
+	console.log("omg i lister to 3000 ocsef on ciojpren ")
 })
 
 
--- route/route.js
+++ route/route.js
@@ -1,67 +1,76 @@
-var express = require('express');
-<<<<<<< HEAD
+
+var express = require("express");
 var router = express.Router();
+var bodyParser = require("body-parser")
 
-var storage = [{
-    id:1,
-    firstname: "Tony",
-    lastname: "Stark"
-}];
-
-router.get('/persons/:id', function (req, res, next) {
-    var id = req.params.id;
-    var person = storage[+id - 1];
-    if (person === null) {
-        var err = new Error('Not Found');
-        err.status = 404;
-        next(err);
-    } else {
-        res.json(person);
-    }
-});
-
-router.get('/test', function (req, res) {
-    res.send('nique bien ta mere');
-});
-
-router.route('/persons').post(function(req, res){
-    var person ={};
-    person.firstname = req.body.firstname;
-    person.lastname = req.body.lastname;
-    person.id = storage.length;
-
-    storage.push(person);
-    res.status(201);
-    res.json(person);
-});
-
-module.exports = router;
-=======
-var ejs = require('ejs');
-var bodyParser = require('body-parser')
-var app = express();
-
-var routes = express.Router();
+var passport = require('passport')
+var TwitterStrategy = require('passport-twitter').Strategy;
 
 var jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-routes.get('/', function (req, res) {
+var TWITTER_CONSUMER_KEY = 'KgaIRtD7PvQhFdIRxZ8RONfQM'
+var TWITTER_CONSUMER_SECRET = 'OQeF0MDulyM1Vx93NJpxoe2SsHIrWRlkJ70ISriodxTPbKkceq'
+
+router.get('/', function (req, res) {
   res.render('homepage.ejs');
 });
 
-routes.post('/', urlencodedParser,function (req, res) {
+//
+
+    passport.use(new TwitterStrategy({
+        consumerKey: TWITTER_CONSUMER_KEY,
+        consumerSecret: TWITTER_CONSUMER_SECRET,
+        callbackURL: "http://localhost/auth/twitter/callback"
+      },
+      function(token, tokenSecret, profile, done) {
+       /* User.findOrCreate(..., function(err, user) {
+          if (err) { return done(err); }
+          done(null, user);
+        });*/
+        User.findOne({
+            'twitter.id': profile.id 
+        }, function(err, user) {
+            if (err) {
+                return done(err);
+            }
+            //No user was found... so create a new user with values from Facebook (all the profile. stuff)
+            if (!user) {
+                user = new User({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    username: profile.username,
+                    provider: 'twitter',
+                    //now in the future searching on User.findOne({'facebook.id': profile.id } will match because of this next line
+                    facebook: profile._json
+                });
+                user.save(function(err) {
+                    if (err) console.log(err);
+                    return done(err, user);
+                });
+            } else {
+                //found user. Return
+                return done(err, user);
+            }
+        });
+      }
+    ));
+router.post('/login_check', urlencodedParser,function (req, res) {
 	console.log("Hello " + req.body.username + " !");
 });
 
-routes.get('/accueil', function (req, res) {
+router.get('/auth/twitter', passport.authenticate('twitter'));
+
+router.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { successRedirect: '/accueil',
+                                     failureRedirect: '/' }));
+
+router.get('/accueil', function (req, res) {
   res.send('PAGE LISTE TWEETS !!!');
 });
 
-routes.get('/TweetSave', function (req, res) {
+router.get('/TweetSave', function (req, res) {
   res.send('PAGE POUR LES TWEETS SAUVER !!!');
 });
 
-
-module.exports = routes;
->>>>>>> f862ffb3285030c55f8c62474be3d515b86a0c8a
+module.exports = router;
